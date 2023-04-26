150f72e6476c3301ce1629b3ad9128f624c7149f
Joycey <Jcheung8385@gmail.com>
2023-04-26 12:59:17 +0100

    Added html so that the definition will render

--- src/components/Meaning.jsx
+++ src/components/Meaning.jsx
@@ -1,5 +1,15 @@
 import styles from "../components/Meaning.module.css";
 const Meaning = (props) => {
-  return <>Meaning</>;
+  if (props.meaning) {
+    return (
+      <div className="Meaning">
+        <h3>{props.meaning.partOfSpeech}</h3>
+        <p>{props.meaning.definition}</p>
+        <em>{props.meaning.example}</em>
+      </div>
+    );
+  } else {
+    return null;
+  }
 };
 export default Meaning;
