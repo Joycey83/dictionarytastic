ddae757a4a1c00e4504b5ed0523cac56974d00be
Joycey <Jcheung8385@gmail.com>
2023-04-26 11:51:45 +0100

    created a searchWord function which will handle the dictionary api

--- src/components/Dictionary.jsx
+++ src/components/Dictionary.jsx
@@ -4,8 +4,11 @@ import styles from "../components/Dictionary.module.css";
 
 const Dictionary = () => {
   const [keyword, setKeyword] = useState("");
+
   const inputKeywordHandle = (event) => {
     setKeyword(event.target.value);
+
+    const searchWord = (event) => {};
   };
   return (
     <>
