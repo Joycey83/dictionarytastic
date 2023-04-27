0c9520eeb7012122a9658658ad458173cbbff82a
Joycey <Jcheung8385@gmail.com>
2023-04-27 14:20:54 +0100

    Added photo images API into the dictionary component

--- src/components/Dictionary.jsx
+++ src/components/Dictionary.jsx
@@ -16,12 +16,22 @@ const Dictionary = () => {
     setDefinition(response.data[0]);
   };
 
+  const showPhotos = (response) => {
+    console.log(response.data);
+    setPhotos(response.data.photos);
+  };
+
   const searchWord = (event) => {
     event.preventDefault();
 
     // Free dictionary API
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     axios.get(apiUrl).then(handleDictionaryResponse);
+
+    // Shecodes Image API
+    let photoApiKey = "a3a5bbeff1t6546488o90285fa5b0853";
+    let photoApiUrl = `https://api.shecodes.io/images/v1/search?query=${keyword}&key=${photoApiKey}`;
+    axios.get(photoApiUrl).then(showPhotos);
   };
   return (
     <>
