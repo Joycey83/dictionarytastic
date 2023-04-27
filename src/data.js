3fb1e070a0d78d87a6d9aeefcb597d06c917828a
Joycey <Jcheung8385@gmail.com>
2023-04-27 13:41:57 +0100

    I will be using the data.js file to import social media icons

--- src/data.js
+++ src/data.js
@@ -1,18 +1,14 @@
 export const images = [
   {
     id: 1,
-    href: "https://1.bp.blogspot.com/-KHgtlLBfvMU/Th3ooqbSzjI/AAAAAAAAA_c/LqrZ1iUa5lY/s1600/scrabble-words%255B1%255D.jpg",
   },
   {
     id: 2,
-    href: "https://connectingpc.files.wordpress.com/2017/12/adobestock_64680360.jpeg?w=1200",
   },
   {
     id: 3,
-    href: "https://i0.wp.com/www.thewordsfallout.com/wp-content/uploads/2015/01/words-image-2a.jpg",
   },
   {
     id: 4,
-    href: "https://www.bemorelnd.co.uk/wp-content/uploads/2020/05/words-text-scrabble-blocks-695571-scaled.jpg",
   },
 ];
