3d451968383dc77496bd35173772e783c91bf3ac
Joycey <Jcheung8385@gmail.com>
2023-04-27 14:34:11 +0100

    Added all the logic for the images

--- src/components/Photos.jsx
+++ src/components/Photos.jsx
@@ -1,4 +1,31 @@
 const Photo = (props) => {
-  return <div>Photo</div>;
+  console.log(props.photos);
+  if (props.photos) {
+    return (
+      <>
+        <div className="Photos">
+          <div className="row">
+            {props.photos.map(function (photo, index) {
+              if (index < 9) {
+                return (
+                  <div className="col-4" key={index}>
+                    <a href={photo.src.original} target="_blank">
+                      <img
+                        src={photo.src.landscape}
+                        alt={photo.alt}
+                        className="img-fluid"
+                      />
+                    </a>{" "}
+                  </div>
+                );
+              }
+            })}{" "}
+          </div>
+        </div>
+      </>
+    );
+  } else {
+    return null;
+  }
 };
 export default Photo;
