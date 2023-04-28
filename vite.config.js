788f78b79ef24c74b067c50d553c25297bc9ab5b
Joycey <Jcheung8385@gmail.com>
2023-04-28 12:33:28 +0100

    undo the plug-in

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-export default ({ mode }) => {
-  return defineConfig({
-    plugins: [react()],
-  });
-};
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+});
