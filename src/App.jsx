83f08fd7b871cff66771e50a3bfe912703bca0e9
Joycey <Jcheung8385@gmail.com>
2023-04-29 13:08:13 +0100

    created a footer component and css module file

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Dictionary from "./components/Dictionary";
 import "./App.css";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
       <div className="app--container">
         <Dictionary />
+        <Footer />
       </div>
     </>
   );
